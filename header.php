9c9e1818944d8959ddc7278c47babcfc50775615
Adrian Szacsko <szacskoadrian@gmail.com>
2023-02-25 15:19:12 +0100

    create header files and blank index file

---
+++ header.php
@@ -0,0 +1,29 @@
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="css/header.css" type="text/css">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@500&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+    </head>
+    <body>
+        <header>
+            <div class="site_name_logo">
+                <img class="logo" src="assets/logo/wtech_logo_v2-removebg-preview.png" alt="logo">
+                <h3 class="website-name">Website name</h3>
+            </div>
+            <nav>
+                <ul class="nav_links">
+                    <li><a href="#"><span class="material-symbols-outlined">search</span> </a></li>
+                    <li><a href="#">Home</a> </li>
+                    <li><a href="#">Shop</a> </li>
+                    <li><a href="#">Contact</a> </li>
+                    <li><a href="#"><span class="material-symbols-outlined">shopping_cart</span> </a></li>
+                    <li><a href="#">Login</a> </li>
+                </ul>
+            </nav>
+        </header>
+    </body>
+</html>
