8e8a8af7c0e07feca404401ce472355bd8d4a1b0
Adrian Szacsko <szacskoadrian@gmail.com>
2023-02-26 12:43:47 +0100

    add footer

--- index.php
+++ index.php
@@ -3,9 +3,13 @@
 <head>
     <meta charset="UTF-8">
     <title>Title</title>
+    <stylesheet class="index.css"></stylesheet>
 </head>
 <body>
 <?php include("header.php"); ?>
-<h1>Hello</h1>
+<div style="height: 800px; width: 100%">
+    <h1 style="text-align: center; ">----CONTENT----</h1>
+</div>
+<?php include("footer.php"); ?>
 </body>
 </html>
