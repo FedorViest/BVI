e016cf7d93058ab750be0d623cd7e2ca90f0c2cd
FedorViest <fedo.viest@gmail.com>
2023-03-08 14:42:52 +0100

    Added box for product and added them to main screen, started working on main screen

---
+++ Components/product_box.php
@@ -0,0 +1,7 @@
+<section class="inner_content">
+    <img class="product_photo" src="../assets/logo/wtech_logo_v2.png" alt="product_image">
+    <section class="product_text">
+        <h3 class="product_name">Product 1</h3>
+        <p class="product_price">19.99€</p>
+    </section>
+</section>
--- index.php
+++ index.php
@@ -10,12 +10,35 @@
     <link rel="stylesheet" href="css/index.css" type="text/css">
     <link rel="stylesheet" href="css/header.css" type="text/css">
     <link rel="stylesheet" href="css/footer.css" type="text/css">
-</head>
+    <link rel="stylesheet" href="css/Components_css/product_box.css" type="text/css">
+    </head>
 <body>
-<?php include("Components/header.php"); ?>
-<div style="height: 815px; width: 100%">
-    <h1 style="text-align: center; ">----CONTENT----</h1>
-</div>
-<?php include("Components/footer.php"); ?>
+    <?php include("Components/header.php"); ?>
+    <section class="content">
+        <section class="welcome_banner">
+            <img class="welcome_image" src="assets/Screenshot_4.png" alt="welcome_image">
+            <h1 class="welcome_text">Welcome to<br>our store</h1>
+        </section>
+        <section class="best_sellers_box">
+            <section class="best_sellers_text">
+                <h2>Best sellers</h2>
+            </section>
+            <section class="best_sellers_products">
+                <table class="products_table">
+                    <tr>
+                        <td><?php include("Components/product_box.php"); ?></td>
+                        <td><?php include("Components/product_box.php"); ?></td>
+                        <td><?php include("Components/product_box.php"); ?></td>
+                        <td><?php include("Components/product_box.php"); ?></td>
+                        <td><?php include("Components/product_box.php"); ?></td>
+                        <td><?php include("Components/product_box.php"); ?></td>
+                        <td><?php include("Components/product_box.php"); ?></td>
+                        <td><?php include("Components/product_box.php"); ?></td>
+                    </tr>
+                </table>
+            </section>
+        </section>
+    </section>
+    <?php include("Components/footer.php"); ?>
 </body>
 </html>
