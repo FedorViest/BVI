ff4a7fec0d7496b7fffa1c2fa7d1079745c3c009
FedorViest <fedo.viest@gmail.com>
2023-03-22 16:25:22 +0100

    Added website name and banner text

--- Components/header.php
+++ Components/header.php
@@ -3,7 +3,7 @@
     <section class="site_name_logo">
         <a href="#" class="website_name_a">
             <img class="logo" src="/assets/logo/wtech_logo_v2-removebg-preview.png" alt="logo">
-            <h3 class="website-name">Website name</h3>
+            <h3 class="website-name">Plant Hub</h3>
         </a>
     </section>
     <form class="search-form">
@@ -12,11 +12,11 @@
     </form>
     <nav class="show_all_nav">
         <ul class="nav_links">
-            <li class="home_button"><a href="#">Home</a> </li>
-            <li><a href="#">Shop</a> </li>
+            <li class="home_button"><a href="index.html">Home</a> </li>
+            <li><a href="shop.html">Shop</a> </li>
             <li><a href="/contact.php">Contact</a> </li>
-            <li><a href="/shopping_cart.html"><span class="material-symbols-outlined">shopping_cart</span> </a></li>
-            <li><a href="#">Login</a> </li>
+            <li><a href="shopping_cart.html"><span class="material-symbols-outlined">shopping_cart</span> </a></li>
+            <li><a href="login.html">Login</a> </li>
         </ul>
         <!-- <section class="dropdown_menu">
             <span class="material-symbols-outlined">menu</span>
@@ -33,11 +33,11 @@
                     <span class="material-symbols-outlined">menu</span>
                 </button>
                 <ul class="dropdown-menu dropdown-menu-end dropdown_content">
-                    <li><a href="#">Home</a> </li>
-                    <li><a href="#">Shop</a> </li>
+                    <li><a href="index.html">Home</a> </li>
+                    <li><a href="shop.html">Shop</a> </li>
                     <li><a href="/contact.php">Contact</a> </li>
-                    <li><a href="/shopping_cart.html">Cart</a> </li>
-                    <li><a href="#">Login</a> </li>
+                    <li><a href="shopping_cart.html">Cart</a> </li>
+                    <li><a href="login.html">Login</a> </li>
                 </ul>
         </div>
     </nav>
