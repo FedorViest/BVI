dd2fb3d2cd93dc21c53dce13bf580026266c361c
AdrianSzacsko <xszacsko@stuba.sk>
2023-04-05 13:27:39 +0200

    add php laravel server

---
+++ public/Components/billing_address_holder.php
@@ -0,0 +1,8 @@
+<!-- Billing section -->
+<section class="billing_section">
+    <Label for="">First Name<br></Label>
+    <label class="input_label">
+        <input type="text" placeholder="Type in your first name...">
+    </label>
+</section>
+<!-- end Billing section -->
---
+++ public/Components/shopping_cart_table_row.php
@@ -0,0 +1,24 @@
+<div class="row d-flex justify-content-around">
+    <div class="col-5 d-inline-block">
+        <div class="row-12 d-flex">
+            <img class="img-thumbnail" src="assets/logo/wtech_logo_v2-removebg-preview.png" alt="logo">
+            <div class="col m-2">
+                <h3>Product 1</h3>
+                <p>Description</p>
+            </div>
+        </div>
+    </div>
+
+    <div class="col-3">
+        <div class="row d-flex justify-content-end">
+            <p class="col-4">5.99€</p>
+            <label class="col-4">
+                <input type="number" class="col-12" required maxlength="2">
+            </label>
+            <div class="col-4">
+                <span class="material-symbols-outlined">delete</span>
+            </div>
+        </div>
+    </div>
+</div>
+<hr>
