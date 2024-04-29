b6655bf939c67f5b76089873250ad9fba4b6fef7
FedorViest <fedo.viest@gmail.com>
2024-04-29 16:30:03 +0200

    update logging logic and update logging data -> turned to json and removed useless data

--- app/Http/Middleware/LogRequests.php
+++ app/Http/Middleware/LogRequests.php
@@ -17,36 +17,24 @@ class LogRequests
      */
     public function handle(Request $request, Closure $next)
     {
-        // Log request information
-        $currentTime = now()->toIso8601String();
-        $timezone = config('app.timezone');
-        $method = $request->method();
-        $path = $request->fullUrl();
-        $user_email = optional(auth()->user())->email;
-        $user_id = optional(auth()->user())->id;
-        $ip = $request->ip();
-        $userAgent = $request->header('User-Agent');
-        $requestHeaders = $request->header();
-        $requestBody = $request->all();
-        $routeName = $request->route()->getName();
-        $controllerAction = $request->route()->getActionName();
-        $sessionId = session()->getId();
-        $responseStatusCode = http_response_code();
-        $startTime = microtime(true);
-        // Your existing code goes here...
-        $endTime = microtime(true);
-        $executionTime = round(($endTime - $startTime) * 1000, 2);
-        $environment = app()->environment();
+        $logData = [
+            'timestamp' => now()->toIso8601String(),
+            'timezone' => config('app.timezone'),
+            'method' => $request->method(),
+            'url' => $request->fullUrl(),
+            'user_email' => optional(auth()->user())->email ?? 'temp user',
+            'user_id' => optional(auth()->user())->id ?? '',
+            'ip_address' => $request->ip(),
+            'route_name' => $request->route()->getName(),
+            'controller_action' => $request->route()->getActionName(),
+            'session_id' => session()->getId(),
+            'status_code' => http_response_code(),
+            'request_headers' => $request->header('User-Agent'),
+            'request_body' => $request->all(),
+        ];
 
-        if ($user_email == null){
-            $user_email = 'temp user';
-        }
-        if ($user_id == null){
-            $user_id = '';
-        }
-
-        $logMessage = "[$currentTime $timezone] [$method] Request from $ip Request to $path by [$user_id $user_email] User Agent: $userAgent, Route: $routeName, Controller Action: $controllerAction, Session ID: $sessionId, Status Code: $responseStatusCode, Execution Time: {$executionTime}ms, Environment: $environment, Request Headers: " . json_encode($requestHeaders) . ", Request Body: " . json_encode($requestBody);
-        Log::channel('requests')->info($logMessage);
+        // Log the data in JSON format
+        Log::channel('requests')->info(json_encode($logData));
 
         return $next($request);
     }
